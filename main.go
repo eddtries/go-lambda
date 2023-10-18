16a1cef08c76f9f2d36f65c7503d979dd55e7ae1
eddtries <eddtries@icloud.com>
2023-10-18 15:07:02 +0200

    refactor into settings.json

--- main.go
+++ main.go
@@ -1,43 +1,62 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
 	"time"
 )
 
-type program struct {
-	path     string
-	language string
+type Programs struct {
+	Programs []Program `json:"programs"`
 }
 
-type schedule struct {
-	program  program
-	interval time.Duration
+type Program struct {
+	Name     string        `json:"name"`
+	Path     string        `json:"path"`
+	Language string        `json:"language"`
+	Interval time.Duration `json:"interval"`
 }
 
-func (program *program) setSchedule(interval time.Duration) schedule {
-	return schedule{program: *program, interval: interval}
+func createProgram(name string, path string, language string, interval int) Program {
+	program := Program{name, path, language, time.Duration(interval) * time.Second}
+	return program
 }
 
-func (schedule *schedule) createTicker() *time.Ticker {
-	return time.NewTicker(schedule.interval)
+func readSettings() Programs {
+	content, err := os.ReadFile("./settings.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var programs Programs
+	err = json.Unmarshal(content, &programs)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return programs
 }
 
-func (program *program) run() {
-	switch program.language {
-	case "Python3":
+func (program *Program) createTicker() *time.Ticker {
+	return time.NewTicker(program.Interval)
+}
+
+func (program *Program) run() {
+	switch program.Language {
+	case "python3":
 		{
-			out, err := exec.Command("python3", program.path).Output()
+			out, err := exec.Command("python3", program.Path).Output()
 			if err != nil {
 				log.Fatal(err)
 			}
 			fmt.Println(string(out))
 		}
-	case "Go":
+	case "golang":
 		{
-			out, err := exec.Command("go", "run", program.path).Output()
+			out, err := exec.Command("go", "run", program.Path).Output()
 			if err != nil {
 				log.Fatal(err)
 			}
@@ -47,26 +66,16 @@ func (program *program) run() {
 }
 
 func main() {
-	pythonProgram1 := program{path: "programs/hello_world.py", language: "Python3"}
-	scheduledProgram1 := pythonProgram1.setSchedule(5 * time.Second)
-	scheduledProgramTicker1 := scheduledProgram1.createTicker()
-
-	pythonProgram2 := program{path: "programs/project_euler_0001.py", language: "Python3"}
-	scheduledProgram2 := pythonProgram2.setSchedule(3 * time.Second)
-	scheduledProgramTicker2 := scheduledProgram2.createTicker()
-
-	goProgram1 := program{path: "programs/bye_world.go", language: "Go"}
-	scheduledProgram3 := goProgram1.setSchedule(6 * time.Second)
-	schedledProgramTicker3 := scheduledProgram3.createTicker()
-
-	for {
-		select {
-		case <-scheduledProgramTicker1.C:
-			scheduledProgram1.program.run()
-		case <-scheduledProgramTicker2.C:
-			scheduledProgram2.program.run()
-		case <-schedledProgramTicker3.C:
-			scheduledProgram3.program.run()
-		}
+	programs := readSettings()
+	for _, program := range programs.Programs {
+		ticker := program.createTicker()
+		go func(ticker *time.Ticker, program Program) {
+			for {
+				select {
+				case <-ticker.C:
+					program.run()
+				}
+			}
+		}(ticker, program)
 	}
 }
